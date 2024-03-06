f8a6c9577b91cc91f7afaaaf4df6ce41df715cf4
Wania Khan <waniak305@gmail.com>
2024-03-06 20:10:58 +0500

    mod2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Module1
+//Module1:
 const gridItems = document.querySelectorAll('.grid-item');
 const array = [...Array(9)].map((_, index) => index + 1); //Array with index from 1 to 9
 function rand() {
@@ -13,3 +13,24 @@ gridItems.forEach((item, index) => {
 item.textContent = array[index]; //Assign the above confirmed shuffled number
 });
 }
+//Module2:
+draggedItem = null;
+function drag(event) {
+  draggedItem = event.target;
+}
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+  function dragOver(event) {
+    event.preventDefault();
+  }
+  
+  function drop(event) {
+    event.preventDefault();
+    if (draggedItem != event.target) {
+        const temp = event.target.textContent;
+        event.target.textContent = draggedItem.textContent;
+        draggedItem.textContent = temp;
+      }
+    }
+    
